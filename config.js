874dce94b4b24aa8c20294d8c74b116d32b6cb0a
CHRIST-DEV-UX-ship <brexyxx@gmail.com>
2025-08-02 12:11:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SNOW~MD~uMdGHL4A#cqF5CF0xoIfBvP-tES3fzIavvDPtuSum9NcGq-5bxB0",
+SESSION_ID: process.env.SESSION_ID || "SNOW~MD~jc0nVIpQ#7XkBZ_b-_BwT1OTQNa1MmYN5SSqQoI8_WI-0HGMnTRM",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -37,9 +37,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50949100359",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "243907687002",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "JON SNOW",
+OWNER_NAME: process.env.OWNER_NAME || "PRINCE HAKIM",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ JON SNOW TECV*",
 // add bot owner name    
